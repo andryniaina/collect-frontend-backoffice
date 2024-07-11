468fdaf1969270c7bdee62e99afaa9d136c44412
Andriniaina RAHERISON <a.raherison@etech.local>
2024-07-11 15:30:13 +0300

    Basic DashBoard

--- src/presentations/layouts/AuthenticatedLayout.tsx
+++ src/presentations/layouts/AuthenticatedLayout.tsx
@@ -1,13 +1,19 @@
-import { Navbar } from "@/components/NavBar";
+import Header from "@/components/header";
+import Sidebar from "@/components/SideBar";
 import { Outlet } from "react-router-dom";
 
 const AuthenticatedLayout = () => {
-    return(
-        <>
-        <Navbar/>
-        <Outlet/>
-        </>
-    )
-}
+  return (
+    <>
+      <Header />
+      <div className="flex h-screen overflow-hidden">
+        <Sidebar />
+        <main className="flex-1 overflow-hidden pt-16">
+          <Outlet />
+        </main>
+      </div>
+    </>
+  );
+};
 
-export default AuthenticatedLayout ;
+export default AuthenticatedLayout;
