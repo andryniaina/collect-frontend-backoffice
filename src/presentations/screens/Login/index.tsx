f8e9e85ba53f3558e6d02c256bd738b40b0d0e3d
i.randimbinirainy <i.randimbinirainy>
2024-07-16 15:30:28 +0300

    Update login

--- src/presentations/screens/Login/index.tsx
+++ src/presentations/screens/Login/index.tsx
@@ -20,7 +20,7 @@ export default function Login() {
   const navigate = useNavigate();
   return (
     <div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
-      <div className="flex items-center justify-center py-12">
+      <div className="flex items-center bg-muted justify-center py-12">
         <div className="mx-auto grid w-[350px] gap-6">
           <div className="grid gap-2 text-center">
             <h1 className="text-3xl font-bold">Login</h1>
@@ -52,7 +52,7 @@ export default function Login() {
           </div>
         </div>
       </div>
-      <div className="hidden bg-muted lg:block">
+      <div className="hidden lg:block">
         <ImageLogin />
       </div>
     </div>
--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -3,10 +3,12 @@ import HomeScreen from "../presentations/screens/HomeScreen";
 import AuthenticatedLayout from "@/presentations/layouts/AuthenticatedLayout";
 import DashBoard from "@/presentations/screens/DashBoard";
 import Project from "@/presentations/screens/DashBoard/Project";
+import Login from "@/presentations/screens/Login";
 
 const MainRoutes = () => {
   return (
     <Routes>
+      <Route path="/login" element={<Login />} />
       <Route path="/" element={<HomeScreen />} />
       <Route path = "/dashboard" element={<AuthenticatedLayout />}>
         <Route path="" element={<DashBoard />} />
